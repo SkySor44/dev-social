9970e3d9f69cf47d8a565d6478c94647ba30e48b
Skyler Sorensen <skylersorensen11@gmail.com>
2019-07-27 16:28:44 -0600

    redux store step up and combineReducers

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index.js';
+
+const initalState = {};
+
+const middleWare = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initalState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
+
+export default store;

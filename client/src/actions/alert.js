41927dc3ed303714fbacd848b4464847744f57aa
Skyler Sorensen <skylersorensen11@gmail.com>
2019-07-27 17:15:34 -0600

    finished alert reducer and action to show alerts dynamically and default to hide after 5 seconds

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,10 +2,19 @@ import uuid from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 //can have dispatch here because of Thunk middleware
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+      }),
+    timeout
+  );
 };
